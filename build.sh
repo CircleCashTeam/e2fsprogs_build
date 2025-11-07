066c2e738ad7f3fa8d05165f113ba0077f959e86
小新大大 <2245062854@qq.com>
2025-11-08 00:50:07 +0800

    Fix build on Linux and Msys gcc g++ support
    Signed-off-by: 小新大大 <2245062854@qq.com>

--- build.sh
+++ build.sh
@@ -2,47 +2,54 @@
 LOCALDIR=$(pwd)
 
 function build() {
-    target="$1"
-
-    if [ -e "build_${target}" ]; then
-        rm -rf "build_${target}"
+    rm -rf "build"
+    if grep -qo 'debian' /etc/os-release; then
+        cmake \
+            -DCMAKE_C_COMPILER=clang \
+            -DCMAKE_CXX_COMPILER=clang++ \
+            -DCMAKE_LINKER=ld.lld \
+            -DCMAKE_SYSTEM_NAME=Linux \
+            -DPREFER_STATIC_LINKING=ON \
+            -DCMAKE_BUILD_TYPE=Debug \
+            -G "Ninja" \
+            -B "build"
     fi
 
-    CC="${target}-clang" \
-    CXX="${target}-clang++" \
-    cmake \
-        -DCMAKE_BUILD_TYPE=Release \
-        -DPREFER_STATIC_LINKING=ON \
-        -DCMAKE_SYSTEM_NAME=Windows \
-        -DCMAKE_LINKER=ld.lld \
-        -B "build_${target}" && \
-    cmake --build "build_${target}" --target=e2fsextract -j$(nproc --all)
+    if uname -o | grep -qo "Msys"; then
+        cmake \
+            -DCMAKE_C_COMPILER=clang \
+            -DCMAKE_CXX_COMPILER=clang++ \
+            -DCMAKE_LINKER=ld.lld \
+            -DCMAKE_SYSTEM_NAME=Windows \
+            -DPREFER_STATIC_LINKING=ON \
+            -DCMAKE_BUILD_TYPE=Debug \
+            -G "Ninja" \
+            -B "build"
+        fi
+
+    cmake --build "build" --target=e2fsextract -j$(nproc --all)
 }
 
 function prepare() {
     if grep -qo 'debian' /etc/os-release; then
-    sudo apt install -y make cmake gcc libc++-dev git patch wget
+        sudo apt install -y cmake gcc clang libc++-dev git ninja-build build-essential wget
         if [[ ! -e "llvm-mingw-20240619-msvcrt-ubuntu-20.04-x86_64" ]]; then
             wget https://github.com/mstorsjo/llvm-mingw/releases/download/20240619/llvm-mingw-20240619-msvcrt-ubuntu-20.04-x86_64.tar.xz
             tar -xf llvm-mingw-20240619-msvcrt-ubuntu-20.04-x86_64.tar.xz -C .
         fi
+        export PATH="$LOCALDIR/llvm-mingw-20240619-msvcrt-ubuntu-20.04-x86_64/bin:$PATH"
     fi
+       
 
     if uname -o | grep -qo "Msys"; then
-    #pacman -Syu --noconfirm
-    pacman -S --noconfirm make cmake gcc git unzip mingw-w64-x86_64-libc++ patch wget
+        pacman -S --noconfirm cmake gcc clang git unzip mingw-w64-x86_64-libc++ wget
         if [[ ! -e "llvm-mingw-20240619-msvcrt-x86_64" ]]; then
-            # wget https://github.com/mstorsjo/llvm-mingw/releases/download/20240619/llvm-mingw-20240619-msvcrt-x86_64.zip
-            unzip llvm-mingw-20240619-msvcrt-x86_64.zip -d .
+            wget https://github.com/mstorsjo/llvm-mingw/releases/download/20240619/llvm-mingw-20240619-msvcrt-x86_64.zip
+            unzip -q llvm-mingw-20240619-msvcrt-x86_64.zip -d .
         fi
+        export PATH="$LOCALDIR/llvm-mingw-20240619-msvcrt-x86_64/bin:$PATH"
     fi
-
-    export PATH="$LOCALDIR/llvm-mingw-20240619-msvcrt-ubuntu-20.04-x86_64/bin:$PATH"
 }
 
 prepare
-for target in "x86_64-w64-mingw32" "i686-w64-mingw32"; do
-    echo "- Building target: ${target} ..."
-    build "$target"
-    echo "- Done!"
-done
+build
