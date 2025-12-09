5c5eafd144dbf5bdef1d4067a12a304e0471656e
小新大大 <2245062854@qq.com>
2025-12-09 20:14:25 +0800

    fix libprocessgroup libprocessgroup_util on linux
    Signed-off-by: 小新大大 <2245062854@qq.com>

--- cmake/include.cmake
+++ cmake/include.cmake
@@ -8,6 +8,8 @@ set(libcutils_headers "${CMAKE_SOURCE_DIR}/src/core/libcutils/include" CACHE STR
 set(libutils_headers "${CMAKE_SOURCE_DIR}/src/core/libutils/include" CACHE STRING "" FORCE)
 set(libutils_headers "${CMAKE_SOURCE_DIR}/src/core/libutils/include" CACHE STRING "" FORCE)
 set(libselinux_headers "${CMAKE_SOURCE_DIR}/src/selinux/libselinux/include" CACHE STRING "" FORCE)
+set(libprocessgroup_util_headers "${CMAKE_SOURCE_DIR}/src/core/libprocessgroup/util/include" CACHE STRING "" FORCE)
+set(libpcre2_headers "${CMAKE_SOURCE_DIR}/src/pcre/include" CACHE STRING "" FORCE)
 set(liblog_headers 
     "${CMAKE_SOURCE_DIR}/src/logging/liblog/include"
     "${CMAKE_SOURCE_DIR}/src/logging/liblog/include_vndk"
@@ -23,4 +25,3 @@ set(libsepol_headers
     "${CMAKE_SOURCE_DIR}/src/selinux/libsepol/cil/include"
     CACHE STRING "" FORCE
 )
-set(libpcre2_headers "${CMAKE_SOURCE_DIR}/src/pcre/include" CACHE STRING "" FORCE)
